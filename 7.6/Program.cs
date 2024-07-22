94d588b581acff6a719e087174e5635535839a06
Giorf81 <rejroberts7@gmail.com>
2024-07-22 21:14:33 +0300

    7.6.12

--- 7.6/Program.cs
+++ 7.6/Program.cs
@@ -9,25 +9,24 @@
     }
     class Engine() { }
     class CarPart() { }
-    class Car<TEngine> where TEngine : Engine
+    abstract class Car<TEngine> where TEngine : Engine
     {
         public TEngine Engine;
 
-        public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart{ }
+        public abstract void ChangePart<TPart>(TPart newPart) where TPart : CarPart{ }
 
     }
-    class ElectricEngine : Engine
+    class ElectricCar : Car<ElectricEngine> 
     {
-
+        public override void ChangePart<TPart>(TPart newPart) { }
     }
-    class GasEngine : Engine
-    {
-    
+    class GasCar: Car<GasEngine> {
+        public override void ChangePart<TPart>(TPart newPart) { }
     }
+    class ElectricEngine : Engine { }
+    class GasEngine : Engine { }
     class Battery() : CarPart{ }
-
     class Differential() : CarPart{ }
-
     class Wheel() : CarPart { }
 
 
